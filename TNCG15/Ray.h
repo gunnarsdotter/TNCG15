6f4a4ed5a1bc7e2b4e2ae0771361e531f4fd5eb0
leognordling .kappa <leo.nordling@insyde.se>
2019-09-24 12:05:15 +0200

    adsa

--- TNCG15/Ray.h
+++ TNCG15/Ray.h
@@ -9,12 +9,14 @@ public:
 	virtual ~Ray() = default;
 	Ray(glm::vec4 start, glm::vec4 end, ColorDbl color):
 		start{ start }, end{ end }, color{ color } {};
-	glm::vec4 getDirection() {
+	glm::vec3 getDirection() {
+		direction = start - end;
 		return start - end;
 	}
 	//Vertex start, end;
 	glm::vec4 start, end;
 	ColorDbl color;
+	glm::vec3 direction;
 	//TODO-Raycontains a reference to the triangle on which theend point is located.
 	//Triangle &t;
 };
