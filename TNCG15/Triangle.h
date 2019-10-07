a05acb1fab4a22454952db8baa58a9a392a2838c
Petra <gunnarsdotter@live.com>
2019-10-07 15:21:45 +0200

    added ray intersection function

--- TNCG15/Triangle.h
+++ TNCG15/Triangle.h
@@ -1,8 +1,8 @@
 #pragma once
-#include "Vertex.h"
+#include "Ray.h"
 #include "ColorDbl.h"
 #include "glm.hpp"
-#include "Direction.h"
+
 class Triangle
 {
 public:
@@ -14,19 +14,9 @@ public:
 	glm::vec3 calculateNormal(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3){
 		return glm::normalize(glm::cross(v2 - v1, v3 - v1));
 	};
-	//TODO-  rayIntersection(Ray arg)
-	//that computes the intersectionbetween aRayand 
-	//theTrianglewith the Möller-Trumbore algorithm
-	glm::vec3 rayIntersection(Ray arg){
-		glm::vec4 T = arg.start - v1;
-		glm::vec4 E1 = v2 - v1;
-		glm::vec4 E2 = v3 - v1;
-		glm::vec4 D = arg.end - arg.start;
-		//glm::vec3 P = glm::cross(D, E2);
-		//glm::vec3 Q = glm::cross(T, E1);
 
-		return v1;
-	}
+	//Computes the intersection point with Möller-Trumbore algorithm
+	bool rayIntersection(Ray arg);
 
 	glm::vec3 getNormal() { return normal; };
 	ColorDbl getColor() {return color;};
